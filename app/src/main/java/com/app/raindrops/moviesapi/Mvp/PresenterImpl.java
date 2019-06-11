55714e4e1cb7dc77d8884b7228fa1b49f611efb0
Rishabh Balani <hrishabh.balani11@gmail.com>
2019-06-11 18:30:40 +0530

    Rxjava dissposable integration for call api every X seconds

--- app/src/main/java/com/app/raindrops/moviesapi/Mvp/PresenterImpl.java
+++ app/src/main/java/com/app/raindrops/moviesapi/Mvp/PresenterImpl.java
@@ -1,15 +1,20 @@
 package com.app.raindrops.moviesapi.Mvp;
 
+import android.util.Log;
+
 import com.app.raindrops.moviesapi.Model.Movie;
 import com.app.raindrops.moviesapi.Retrofit.APIInterface;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
+import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 public class PresenterImpl implements MainActivtyContract.presenter {
@@ -17,7 +22,7 @@ public class PresenterImpl implements MainActivtyContract.presenter {
 
     private APIInterface apiInterface;
     private MainActivtyContract.view view;
-
+    private  Disposable disposable;
     @Inject
     public PresenterImpl(APIInterface apiInterface, MainActivtyContract.view view) {
         this.apiInterface = apiInterface;
@@ -26,6 +31,14 @@ public class PresenterImpl implements MainActivtyContract.presenter {
 
     @Override
     public void loadData() {
+        callapi();
+        //For calling api every 5 second we can use Dispossable
+        //comment callapi method when uncomment callapiwithdissposable()
+       // callapiwithdissposable();
+    }
+
+    private void callapi() {
+        //Thread management is done automatically in rxjava
         apiInterface.getMovies().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<List<Movie>>() {
             @Override
             public void onSubscribe(Disposable d) {
@@ -48,5 +61,24 @@ public class PresenterImpl implements MainActivtyContract.presenter {
                 view.hideProgress();
             }
         });
+    }
+    private void callapiwithdissposable(){
+        disposable = Observable.interval(1000, 5000, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        //  callEndpoints();
+                       // Boko();
+                        callapi();
+                        Log.e("APIcalled", "Api called");
+
+                    }
+                });
+
+        //if user want to end api call
+        //disposable.dispose();
+
     }
 }
